8275b182fca9f5514af901bd6ecfa827ef4526cd
padiweb <padiweb@gmail.com>
2026-09-06 23:30:11 +0000

    fix: versionCode 20 untuk Play Store, v1.2.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "id.padiweb.eduweb"
         minSdk = 21
         targetSdk = 36
-        versionCode = 19
-        versionName = "1.2.5"
+        versionCode = 20
+        versionName = "1.2.6"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
