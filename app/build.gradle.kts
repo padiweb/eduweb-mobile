f2102de15891a9da7b0c6c181b1f6e7222af467d
padiweb <padiweb.dev@gmail.com>
2026-09-06 15:26:32 +0700

    fix: versionCode 17 (Play Console tidak izinkan pakai ulang versionCode lama), v1.2.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "id.padiweb.eduweb"
         minSdk = 21
         targetSdk = 36
-        versionCode = 16
-        versionName = "1.2.2"
+        versionCode = 17
+        versionName = "1.2.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
