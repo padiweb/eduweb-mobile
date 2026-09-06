3a1793186b1d95fda0e7ecd5ae2b8c85a70854b7
padiweb <padiweb@gmail.com>
2026-09-06 09:14:04 +0000

    fix: tambah deklarasi tidak pakai AD_ID untuk syarat Play Store, v1.2.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "id.padiweb.eduweb"
         minSdk = 21
         targetSdk = 36
-        versionCode = 17
-        versionName = "1.2.3"
+        versionCode = 18
+        versionName = "1.2.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
